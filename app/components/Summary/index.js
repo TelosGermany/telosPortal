96852082af335d4c5b567c60e209d27f3355df7b
Andre Litty <andre.litty@gmx.de>
2018-12-15 12:24:15 +1100

    Removed some text, fixed some minor issues for Telos compability

--- app/components/Summary/index.js
+++ app/components/Summary/index.js
@@ -20,7 +20,7 @@ import { makeSelectActiveNetwork, makeSelectAccount } from 'containers/NetworkCl
 
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import Announcement from '@material-ui/icons/Announcement';
-import Warning from "components/Typography/Warning.jsx";
+import Warning from 'components/Typography/Warning.jsx';
 // core components
 import GridContainer from 'components/Grid/GridContainer';
 import GridItem from 'components/Grid/GridItem';
@@ -34,34 +34,29 @@ import userProfileStyles from './comingSoon';
 
 function Summary(props) {
   const { classes, account, network } = props;
-    return (
-      <div>
-        <GridContainer>
-          <GridItem xs={12} sm={12} md={12}>
-            <Card>
-              <CardHeader icon>
-                <CardIcon color="success">
-                  <AccountCircle />
-                </CardIcon>
-                <h5 className={classes.cardIconTitle}>
-                  {account ? (
-                    `${account.account_name} [${network.network.name} via ${network.endpoint.name}]`
-                  ) : ('Attach an Account')}
-
-                </h5>
-              </CardHeader>
-              <CardBody>
-
-                <ResourceTable account={account} />
-                <Warning>
-                  <h6><Announcement/> Token details powered by eosflare and greymass</h6>
-                </Warning>
-              </CardBody>
-            </Card>
-          </GridItem>
-        </GridContainer>
-      </div>
-    );
+  return (
+    <div>
+      <GridContainer>
+        <GridItem xs={12} sm={12} md={12}>
+          <Card>
+            <CardHeader icon>
+              <CardIcon color="success">
+                <AccountCircle />
+              </CardIcon>
+              <h5 className={classes.cardIconTitle}>
+                {account
+                  ? `${account.account_name} [${network.network.name} via ${network.endpoint.name}]`
+                  : 'Attach an Account'}
+              </h5>
+            </CardHeader>
+            <CardBody>
+              <ResourceTable account={account} />
+            </CardBody>
+          </Card>
+        </GridItem>
+      </GridContainer>
+    </div>
+  );
 }
 
 const mapStateToProps = createStructuredSelector({
