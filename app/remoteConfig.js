3770fb3fbb368a801e0a2ef88ba579cbc37538ee
Andre Litty <andre.litty@gmx.de>
2019-01-26 21:51:05 +1100

    Added viitasphere tokens

--- app/remoteConfig.js
+++ app/remoteConfig.js
@@ -4,7 +4,24 @@
 
 // TODO: Remove unused airgrabs and add new airgrabs
 
-const airgrabs = [];
+const airgrabs = [
+  {
+    symbol: 'VIITA',
+    account: 'viitasphere1',
+    method: 'signup',
+    description:
+      'Viitasphere is the economy of the future, where everyday people, rather than a central power, control the economy. With Viitasphere every individual will have the ability to connect with one another on a peer-to-peer platform in order to request and provide services and goods for a cost determined by both parties, paid in any manner they see fit.',
+    url: 'http://www.viitasphere.com/',
+  },
+  {
+    symbol: 'VIICT',
+    account: 'viitasphere1',
+    method: 'signup',
+    description:
+      'Viitasphere is the economy of the future, where everyday people, rather than a central power, control the economy. With Viitasphere every individual will have the ability to connect with one another on a peer-to-peer platform in order to request and provide services and goods for a cost determined by both parties, paid in any manner they see fit.',
+    url: 'http://www.viitasphere.com/',
+  },
+];
 
 const claimsUrl = 'https://raw.githubusercontent.com/eostoolkit/eos-claims/master/claims.json-DISABLED';
 const networksUrl = 'https://raw.githubusercontent.com/TelosGermany/network_list/master/networks.json';
