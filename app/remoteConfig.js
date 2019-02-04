f7b38c9919d63862dadfec8ea887bcb6ee0fe1a9
Andre Litty <andre.litty@gmx.de>
2019-02-04 21:55:08 +1100

    Added viitasphere airgrabs

--- app/remoteConfig.js
+++ app/remoteConfig.js
@@ -8,15 +8,7 @@ const airgrabs = [
   {
     symbol: 'VIITA',
     account: 'viitasphere1',
-    method: 'signup',
-    description:
-      'Viitasphere is the economy of the future, where everyday people, rather than a central power, control the economy. With Viitasphere every individual will have the ability to connect with one another on a peer-to-peer platform in order to request and provide services and goods for a cost determined by both parties, paid in any manner they see fit.',
-    url: 'http://www.viitasphere.com/',
-  },
-  {
-    symbol: 'VIICT',
-    account: 'viitasphere1',
-    method: 'signup',
+    method: 'open',
     description:
       'Viitasphere is the economy of the future, where everyday people, rather than a central power, control the economy. With Viitasphere every individual will have the ability to connect with one another on a peer-to-peer platform in order to request and provide services and goods for a cost determined by both parties, paid in any manner they see fit.',
     url: 'http://www.viitasphere.com/',
