4bd9adc46858a84cca1e4872b938c984dfc51c29
Andre Litty <andre.litty@gmx.de>
2019-07-13 21:51:23 +1000

    Removed hard coded custom token

--- app/remoteConfig.js
+++ app/remoteConfig.js
@@ -11,28 +11,4 @@ const networksUrl = 'https://raw.githubusercontent.com/TelosGermany/network_list
 const refUrl = 'https://s3.amazonaws.com/api.eosvotes.io/eosvotes/tallies/latest.json';
 const tokensUrl = 'https://raw.githubusercontent.com/TelosGermany/token_list/master/tokens.json';
 
-const customTokens = [
-  {
-    name: 'Cnt',
-    logo: '',
-    logo_lg: '',
-    symbol: 'CNT',
-    account: 'vapaeetokens',
-  },
-  {
-    name: 'Acorn',
-    logo: '',
-    logo_lg: '',
-    symbol: 'ACORN',
-    account: 'acornaccount',
-  },
-  {
-    name: 'Robo',
-    logo: '',
-    logo_lg: '',
-    symbol: 'ROBO',
-    account: 'proxibotstkn',
-  },
-];
-
 export { refUrl, networksUrl, claimsUrl, airgrabs, tokensUrl };
